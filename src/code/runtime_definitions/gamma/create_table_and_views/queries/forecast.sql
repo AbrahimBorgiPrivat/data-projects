02825c04ac6cd09e33cc9acf9e92c0b1b45f5ba7
AbrahimBorgiPrivat <Abrahim.borgi@live.dk>
2025-10-23 23:00:04 +0200

    Made folder adjustement to ETL services

---
+++ src/code/runtime_definitions/gamma/create_table_and_views/queries/forecast.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS gamma_db.forecast
+(
+    id text COLLATE pg_catalog."default" NOT NULL,
+    account_id text COLLATE pg_catalog."default",
+    postering_group_id text COLLATE pg_catalog."default",
+    year_actual bigint,
+    forecast double precision,
+    CONSTRAINT forecast_pkey PRIMARY KEY (id)
+)
