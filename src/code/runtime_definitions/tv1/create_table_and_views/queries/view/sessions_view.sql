db6c3b4c8b88494a618b0f3acda7ca602096b55e
AbrahimBorgiPrivat <Abrahim.borgi@live.dk>
2025-11-11 01:19:45 +0100

    Created starting point for TVone Rapport

--- src/code/runtime_definitions/tv1/create_table_and_views/queries/view/sessions_view.sql
+++ src/code/runtime_definitions/tv1/create_table_and_views/queries/view/sessions_view.sql
@@ -1,9 +1,10 @@
-CREATE OR REPLACE VIEW tv_one.sessions_view AS
-SELECT
-    tracking_id      AS session_tracking_id,
-    session_id,
-    tracking_time    AS session_start_time,
-    platform,
-    user_id
-FROM tv_one.tracking
-WHERE type = 'Session starts';
+CREATE OR REPLACE VIEW tv_one.sessions_view
+ AS
+ SELECT tracking.tracking_id AS session_tracking_id,
+    tracking.session_id,
+    tracking.tracking_time AS session_start_time,
+    tracking.tracking_time::date AS session_start_date,
+    tracking.platform,
+    tracking.user_id
+   FROM tv_one.tracking
+  WHERE tracking.type = 'Session starts'::text
